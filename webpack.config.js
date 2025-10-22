2e357cba53f6e7352dfdb387dcc9378b4df4cf74
gabrielmagevski <gabrielmagevski.gs@gmail.com>
2025-10-22 17:21:09 -0300

    config project with react

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,4 @@
 const path = require("path");
-const webpack = require("webpack");
 
 module.exports = (paths) => ({
   entry: {
@@ -10,15 +9,16 @@ module.exports = (paths) => ({
     filename: "bundle.js",
   },
   mode: "development",
+  resolve: {
+    extensions: [".tsx", ".ts", ".js", ".jsx"],
+  },
   module: {
     rules: [
       {
-        test: /\.(js|jsx|ts|tsx)$/,
-        exclude: /(node_modules|bower_components)/,
-        include: path.resolve(__dirname, paths.scripts.src),
+        test: /\.(ts|tsx|js|jsx)$/,
+        exclude: /node_modules/,
         use: "ts-loader",
       },
     ],
   },
-  plugins: [],
 });
